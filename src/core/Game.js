b5b12c625082bbe1b5b6df2b32c2d5d5bd008b80
Greg Spratling <gregspratling@imap.cc>
2026-08-09 22:49:11 +1000

    Mild character texture changes

--- src/core/Game.js
+++ src/core/Game.js
@@ -109,8 +109,8 @@ export class Game {
       this.scene,
       this.bulletSystem.bullets,
       this.blastSystem.blasts);
-    this.player1.group.position.x = 10;
-    this.player1.group.position.z = 10;
+    this.player1.group.position.x = -8;
+    this.player1.group.position.z = -8;
     
     this.player2 = new Player(
       "Player 2",
