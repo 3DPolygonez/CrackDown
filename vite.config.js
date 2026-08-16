8126bbb7d4d4daffffd60eaae880f21547443aa3
Greg Spratling <gregspratling@imap.cc>
2026-08-16 15:59:13 +1000

    CHanges to vite.config.js to ensure all .html files are put in the /dist folder on build

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,15 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
-    base: '/CrackDown/'
-})
+    base: '/CrackDown/',
+
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                character: resolve(__dirname, 'character.html')
+            }
+        }
+    }
+});
