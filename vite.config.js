b11249903f92dd531d4df6ae73ffef7e9659707b
Greg Spratling <gregspratling@imap.cc>
2026-08-25 07:06:06 +1000

    vite config fix for paths

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
             input: {
                 main: resolve(__dirname, 'index.html'),
                 character: resolve(__dirname, 'character.html'),
-                character: resolve(__dirname, 'path.html')
+                path: resolve(__dirname, 'path.html')
             }
         }
     }
